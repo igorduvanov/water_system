851fe959b814617775fca7d9fa31413d6d051d40
Igor Duvanov <duvanovigor79@gmail.com>
2021-02-17 10:16:20 +0200

    ініціалізація git для проекту

---
+++ header.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css.map">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/main.css">
+
+
+
+</head>
+<body>
+<header>
+   <div class="container">
+       <div class="row">
+           <div class="col">
+
+               <nav class="navbar navbar-expand-lg navbar-light ">
+                   <a class="navbar-brand" href="#">
+                       <img src="img/logo.png" alt="main logo" class="logo">
+                   </a>
+                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                       <span class="navbar-toggler-icon"></span>
+                   </button>
+                   <div class="collapse navbar-collapse" id="navbarNav">
+                       <ul class="navbar-nav">
+                           <li class="nav-item ">
+                               <a class="nav-link" href="#">Наші перваги </a>
+                           </li>
+                           <li class="nav-item">
+                               <a class="nav-link" href="#">Питання та відповіді</a>
+                           </li>
+                           <li class="nav-item">
+                               <a class="nav-link" href="#">Документи</a>
+                           </li>
+                           <li class="nav-item">
+                               <a class="nav-link " href="#" >Допомога</a>
+                           </li>
+                       </ul>
+                       <button type="button" class="btn  my_btn btn_room">Мій кабінет</button>
+                       <button type="button" class="btn  my_btn btn_register nav-stacked nav-pills">Зареєструватися</button>
+                   </div>
+               </nav>
+           </div>
+       </div>
+       <div class="row">
+           <div class="col-8">
+               <div class="offer">
+                   <h1 class="display-4 offer-head">Здіснюй подачу показників води легко та швидко, без жодних зусиль</h1>
+                   <p class="lead offer-desk">Система автоматизованої подачі показників, зекономить Ваш час та дозволить легко та швидко здійснювати оплату за надані послуги</p>
+                   <a class="btn btn-primary btn-lg my_btn offer-btn" href="#" role="button">Спробувати зараз</a>
+               </div>
+           </div>
+           <div class="col-4 offer-img">
+               <img src="img/main-image.png" alt="main-image">
+           </div>
+       </div>
+   </div>
+
+</header>
+
---
+++ index.php
@@ -0,0 +1,103 @@
+<?php
+include_once ('header.php');
+?>
+<section class="permision" xmlns="http://www.w3.org/1999/html">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h2 class="permision-head">Переваги нашої системи</h2>
+            </div>
+        </div>
+        <div class="row row-cols-1 row-cols-md-4">
+            <div class="col ">
+                <div class="card h-100">
+                    <img src="img/image 5.png" class="card-img-top permision-img rounded mx-auto d-block" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title permision-title">Зручна оплата </h5>
+                        <p class="card-text permision-text">Сплачуйте за послуги в будь-який час доби</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col ">
+                <div class="card h-100">
+                    <img src="img/image 6.png" class="card-img-top permision-img rounded mx-auto d-block" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title permision-title">Історія платежів</h5>
+                        <p class="card-text permision-text">Перегляд історії показників лічильників та перегляд звітів</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col ">
+                <div class="card h-100 ">
+                    <img src="img/image 4.png" class="card-img-top permision-img rounded mx-auto d-block" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title permision-title">Повний крнтроль</h5>
+                        <p class="card-text permision-text">Контроль нарахувань та оплати по особовому рахунку</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col ">
+                <div class="card h-100">
+                    <img src="img/image 3.png" class="card-img-top permision-img rounded mx-auto d-block" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title permision-title">Особистий кабінет</h5>
+                        <p class="card-text permision-text">Перегляд та опалата рахунків за декількома адресама</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+<section class="registration">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h2 class="register-head">Проста та швидка реєстрація</h2>
+            </div>
+        </div>
+        <div class="rov">
+            <div class="col">
+                <div class="card-deck">
+                    <div class="card">
+                        <img src="img/image 7.png" class="card-img-top register-img rounded mx-auto d-block" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title register-title">Крок 1: <span>Зареєструйтеся</span></h5>
+                            <p class="card-text register-text">Введіть свою електронну адресу та пароль</p>
+
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="img/image 8.png" class="card-img-top register-img rounded mx-auto d-block" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title register-title">Крок 2:<span> Підтвердіть свій email</span></h5>
+                            <p class="card-text register-text">Підтвердіть свої дані у поштовій скринці та перейдіть у свій особистий кабінет</p>
+
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img src="img/image 9.png" class="card-img-top register-img rounded mx-auto d-block" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title register-title">Крок 3:<span> Додайте особовий рахунок</span></h5>
+                            <p class="card-text register-text">Введіть свій особовий рахунок, який знаходиться на квитанції</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <form class="form-inline form justify-content-center ">
+                    <div class="form-group  ">
+                        <input type="email" class="form-control use-form " id="inputPassword2" placeholder="Введіть email ">
+                    </div>
+                    <button type="submit" class="btn btn-primary my_btn form-btn ">Зареєструватися</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+    include_once ('footer.php');
+?>
